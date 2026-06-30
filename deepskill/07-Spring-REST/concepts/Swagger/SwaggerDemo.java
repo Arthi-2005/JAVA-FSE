1244747a58bc2300745dbb790e39041779a5fd7b
Arthi Jayasankar <arthi@example.com>
2026-06-30 19:45:00 +0530

    07 - Swagger concept demo

---
+++ deepskill/07-Spring-REST/concepts/Swagger/SwaggerDemo.java
@@ -0,0 +1,20 @@
+// Swagger = API Documentation (auto-generated)
+// Gives you a UI to test your APIs
+
+public class SwaggerDemo {
+    public static void main(String[] args) {
+        System.out.println("=== Swagger / OpenAPI ===");
+        System.out.println();
+        System.out.println("Access: http://localhost:8080/swagger-ui.html");
+        System.out.println();
+        System.out.println("What Swagger shows:");
+        System.out.println("  - All endpoints (GET, POST, PUT, DELETE)");
+        System.out.println("  - Request format (JSON body)");
+        System.out.println("  - Response format");
+        System.out.println("  - Status codes");
+        System.out.println("  - Test APIs directly from browser!");
+        System.out.println();
+        System.out.println("Dependency in pom.xml:");
+        System.out.println("  springdoc-openapi-starter-webmvc-ui");
+    }
+}
