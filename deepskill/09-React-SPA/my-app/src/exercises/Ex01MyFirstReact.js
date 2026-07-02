2bc6075da6cceb1ddfdf2e8b68e31048fe626a20
Arthi Jayasankar <arthi@example.com>
2026-07-02 15:25:00 +0530

    09 - Ex01: My First React

---
+++ deepskill/09-React-SPA/my-app/src/exercises/Ex01MyFirstReact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Ex01MyFirstReact() {
+  return (
+    <div className="exercise-wrap">
+      <h1>welcome to first session of React</h1>
+    </div>
+  );
+}
+
+export default Ex01MyFirstReact;
